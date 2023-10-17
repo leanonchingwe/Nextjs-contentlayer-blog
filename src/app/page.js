9bfd116bf7c9901de6e06e28d1f6bbe83a140a4d
leanonchingwe <66792863+leanonchingwe@users.noreply.github.com>
2023-10-17 08:38:40 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 // pages/page.js
 
 import React from 'react';
+import styles from './page.module.css'; // Import a CSS module
 
 const Page = () => {
   return (
-    <div>
+    <div className={styles.pageContainer}>
       <h1>This is the Page component</h1>
       <p>This is a basic Next.js page.</p>
     </div>
